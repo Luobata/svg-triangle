d9d1258c417cf8e7714d15d535d36876e409c408
yaochenghao <yaochenghao@bytedance.com>
2018-08-21 21:01:28 +0800

    add: test-case

--- test/index.js
+++ test/index.js
@@ -2,42 +2,45 @@ import Triangle from '../src/';
 
 window.onload = () => {
     const svg = new Triangle({
-        width: 200,
-        height: 100,
-        shadow: 5,
-        radius: 10,
+        width: 20,
+        height: 10,
+        shadow: 0,
+        radius: 2,
         color: 'yellow',
         border: 'red',
+        className: 'triangle',
     });
     const svg2 = new Triangle({
         width: 200,
         height: 100,
         shadow: 5,
-        radius: 10,
+        radius: 20,
         color: 'yellow',
         border: 'red',
         direction: 'up',
     });
     const svg3 = new Triangle({
-        width: 200,
-        height: 100,
-        shadow: 5,
-        radius: 10,
+        width: 20,
+        height: 10,
+        shadow: 1,
+        radius: 2,
         color: 'yellow',
         border: 'red',
         direction: 'right',
     });
     const svg4 = new Triangle({
-        width: 200,
-        height: 100,
-        shadow: 5,
-        radius: 10,
+        width: 20,
+        height: 10,
+        shadow: 1,
+        radius: 2,
         color: 'yellow',
         border: 'red',
         direction: 'left',
     });
 
-    document.body.appendChild(svg.triangle);
+    const wrap = document.querySelector('.wrap');
+    wrap.append(svg.triangle);
+    // document.body.appendChild(svg.triangle);
     document.body.appendChild(svg2.triangle);
     document.body.appendChild(svg3.triangle);
     document.body.appendChild(svg4.triangle);
